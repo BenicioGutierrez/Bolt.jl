24f78b808acf6534527f6a7e92e0df5eb8a879f6
Benicio Gutierrez <106838587+BenicioGutierrez@users.noreply.github.com>
2024-08-29 22:48:44 -0600

    Update background.jl
    Add energy density omega_a

--- src/background.jl
+++ src/background.jl
@@ -1,4 +1,3 @@
-
 # NOTE: Bolt's background functions are in terms of x ≡ ln(a), the log scale factor
 
 const ζ = 1.2020569 #Riemann ζ(3) for phase space integrals
@@ -11,6 +10,7 @@ function Ω_Λ(par::AbstractCosmoParams)
     νfac =(90 * ζ /(11 * π^4)) * (par.Ω_r * par.h^2 / Tγ) *((par.N_ν/3)^(3/4))
     #^the factor that goes into nr approx to neutrino energy density, plus equal sharing ΔN_eff factor for single massive neutrino
     Ω_ν = par.Σm_ν*νfac/par.h^2
+
     return 1 - (par.Ω_r*(1+(2/3)*(7par.N_ν/8)*(4/11)^(4/3))  # dark energy density
                                          + par.Ω_b + par.Ω_c
                                          + Ω_ν
@@ -57,10 +57,12 @@ end
 # Hubble parameter ȧ/a in Friedmann background
 function H_a(a, par::AbstractCosmoParams,quad_pts,quad_wts)
     ρ_ν,_ = ρP_0(a,par,quad_pts,quad_wts) #FIXME dropped pressure, need to decide if we want it for tests?
-    return H₀(par) * √((par.Ω_c + par.Ω_b ) * a^(-3)
+    log_a = log10(a)
+    return H₀(par) *(1 + par.Ω_new(log_a)) * √((par.Ω_c + par.Ω_b ) * a^(-3)
                         + ρ_ν/ρ_crit(par)
                         + par.Ω_r* a^(-4)*(1+(2/3)*(7par.N_ν/8)*(4/11)^(4/3))
-                        + Ω_Λ(par))
+                        + Ω_Λ(par)
+                        )
 end
 # conformal time Hubble parameter, aH
 ℋ_a(a, par::AbstractCosmoParams,quad_pts,quad_wts) = a * H_a(a, par,quad_pts,quad_wts)
@@ -86,7 +88,6 @@ struct Background{T, IT, GT} <: AbstractBackground{T, IT, GT}
     H₀::T
     η₀::T
     ρ_crit::T
-    Ω_Λ::T
 
     x_grid::GT
     quad_pts::Array{T,1}
@@ -110,7 +111,6 @@ function Background(par::AbstractCosmoParams{T}; x_grid=-20.0:0.01:0.0, nq=15) w
         T(H₀(par)),
         T(η(0.0, par,quad_pts,quad_wts)),
         T(ρ_crit(par)),
-        T(Ω_Λ(par)),
 
         x_grid,
         convert(Array{T,1},quad_pts), #explicit call to convert instead of constructor for arrays
